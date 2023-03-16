e38add83210de64d5b00b136ce3d88d046b6b168
PriskaSama <106514351+PriskaSama@users.noreply.github.com>
2023-03-16 08:55:51 +0100

    removed unused code

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -1,6 +1,5 @@
-import { motion } from "framer-motion";
 import React from "react";
-import { Audio, Hearts } from  "react-loader-spinner";
+import { Hearts } from  "react-loader-spinner";
 
 export const Loader = () => {
   return (
@@ -15,21 +14,6 @@ export const Loader = () => {
           visible={true}
           />
         </div>
-    // <motion.div
-    //   className="w-40 h-40 bg-black/90"
-    //   animate={{
-    //     scale: [1, 2, 2, 1, 1],
-    //     rotate: [0, 0, 180, 180, 0],
-    //     borderRadius: ["0%", "0%", "50%", "50%", "0%"]
-    //   }}
-    //   transition={{
-    //     duration: 2,
-    //     ease: "easeInOut",
-    //     times: [0, 0.2, 0.5, 0.8, 1],
-    //     repeat: Infinity,
-    //     repeatDelay: 1
-    //   }}
-    // />
   );
 };
 
